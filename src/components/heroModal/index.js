eb9e8343f90816b17039f2e1a17c1522a0d378ed
BALAJI141031 <85923874+BALAJI141031@users.noreply.github.com>
2022-04-08 21:36:48 +0530

    refactor:video height changed

--- src/components/heroModal/index.js
+++ src/components/heroModal/index.js
@@ -6,19 +6,19 @@ function HeroBanner({ hero, category }) {
   // console.log(category, "===ghgghghh");
 
   return (
-      <div className={hero ? "banner hero-banner-width" : "banner"}>
-        <h1 className="heading-margin">
-          {hero ? "Spark Library" : category.categoryName}
-        </h1>
-        {hero && (
-          <h2 className="heading-margin">
-            Dedicated International Relations Niche To Civils Aspirant's
-          </h2>
-        )}
-        {category && <h2>{category.description}</h2>}
-        <button>Explore</button>
-      </div>
-    );
+    <div className={hero ? "banner hero-banner-width" : "banner"}>
+      <h1 className="heading-margin">
+        {hero ? "Spark Library" : category.categoryName}
+      </h1>
+      {hero && (
+        <h2 className="heading-margin" style={{ fontSize: "1.5rem" }}>
+          Dedicated International Relations Niche To Civils Aspirant's
+        </h2>
+      )}
+      {category && <h2>{category.description}</h2>}
+      <button>Explore</button>
+    </div>
+  );
 }
 
 export { HeroBanner };
