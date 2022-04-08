c78d1ac2484de97a34751dd2fe6e9bf522e1d423
BALAJI141031 <85923874+BALAJI141031@users.noreply.github.com>
2022-04-08 20:58:29 +0530

    refactor:home page refactored

--- src/components/heroModal/index.js
+++ src/components/heroModal/index.js
@@ -1,15 +1,24 @@
 import "./index.css";
 
-function HeroBanner({ hero }) {
+function HeroBanner({ hero, category }) {
+  // console.log(props, "show me atleast props");
+
+  // console.log(category, "===ghgghghh");
+
   return (
-    <div className={hero ? "banner hero-banner-width" : "banner"}>
-      <h1 className="heading-margin">Spark Library</h1>
-      <h2 className="heading-margin">
-        Dedicated International Relations Niche To Civils Aspirant's{" "}
-      </h2>
-      <button>Explore</button>
-    </div>
-  );
+      <div className={hero ? "banner hero-banner-width" : "banner"}>
+        <h1 className="heading-margin">
+          {hero ? "Spark Library" : category.categoryName}
+        </h1>
+        {hero && (
+          <h2 className="heading-margin">
+            Dedicated International Relations Niche To Civils Aspirant's
+          </h2>
+        )}
+        {category && <h2>{category.description}</h2>}
+        <button>Explore</button>
+      </div>
+    );
 }
 
 export { HeroBanner };
